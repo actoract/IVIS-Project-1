d56ea2b742e88b7a76e739d70d1493243e3d0e65
anna <anna>
2022-01-25 16:22:13 +0100

    updated for radar chart: color, clear button

--- public/skills.js
+++ public/skills.js
@@ -1,9 +1,191 @@
+var participants = [
+  {label: "Value_1", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [2,3,2,5,7,5,2,5,8,9,6,3]
+  },
+  {label: "Value_2", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [4,4,5,4,6,7,3,2,8,10,10,7]
+  },
+  {label: "Value_3", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [4,4,6,2,7,5,5,5,7,10,9,5]
+  },
+  {label: "Value_4", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [4,5,5,6,7,7,3,7,2,8,8,9]
+  },
+  {label: "Value_5", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [6,8,8,4,7,7,6,6,7,6,8,6]
+  },
+  {label: "Value_6", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,3,3,2,6,2,2,3,3,9,8,3]
+  },
+  {label: "Value_7", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,6,7,6,9,5,3,4,4,9,10,6]
+  },
+  {label: "Value_8", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [1,3,4,3,7,5,6,3,2,8,8,5]
+  },
+  {label: "Value_9", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [5,6,5,5,8,6,7,5,4,8,6,7]
+  },
+  {label: "Value_10", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [2,6,1,7,5,2,2,2,4,8,7,3]
+  },
+  {label: "Value_11", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [2,4,5,7,7,3,2,3,7,4,5,5]
+  },
+  {label: "Value_12", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,3,3,6,9,5,2,3,3,8,8,4]
+  },
+  {label: "Value_13", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,2,5,7,5,4,2,2,8,9,8,6]
+  },
+  {label: "Value_14", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,4,7,2,7,6,4,3,2,5,5,6]
+  },
+  {label: "Value_15", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [5,8,8,3,8,5,5,6,5,7,7,9]
+  },
+  {label: "Value_16", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [8,9,7,5,9,7,6,5,6,7,6,7]
+  },
+  {label: "Value_17", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,4,5,8,4,7,5,3,4,8,7,7]
+  },
+  {label: "Value_18", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,3,6,8,8,5,7,6,5,9,9,4]
+  },
+  {label: "Value_19", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [5,7,6,4,8,9,5,6,3,5,7,8]
+  },
+  {label: "Value_20", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [5,7,6,1,8,7,7,7,7,5,7,8]
+  },
+  {label: "Value_21", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [6,6,7,8,8,6,6,5,5,10,9,8]
+  },
+  {label: "Value_22", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [7,4,5,2,9,8,3,4,3,7,6,9]
+  },
+  {label: "Value_23", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [1,1,6,3,3,6,2,1,1,4,4,4]
+  },
+  {label: "Value_24", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [1,4,5,4,9,3,1,2,2,5,7,4]
+  },
+  {label: "Value_25", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,6,5,1,7,6,3,5,5,8,7,8]
+  },
+  {label: "Value_26", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [2,3,7,3,8,8,7,3,3,7,8,8]
+  },
+  {label: "Value_27", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [2,4,5,4,7,5,5,3,3,4,5,4]
+  },
+  {label: "Value_28", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [1,3,7,10,6,1,4,2,3,7,8,5]
+  },
+  {label: "Value_29", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [1,3,5,2,7,4,1,1,3,10,8,8]
+  },
+  {label: "Value_30", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [1,4,4,10,10,2,1,4,6,8,10,2]
+  },
+  {label: "Value_31", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [5,3,8,3,9,8,5,4,2,9,5,8]
+  },
+  {label: "Value_32", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [2,3,8,8,8,7,1,5,5,8,7,8]
+  },
+  {label: "Value_33", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [2,3,7,2,8,6,3,4,3,5,6,6]
+  },
+  {label: "Value_34", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [2,1,5,2,7,4,3,3,3,5,5,5]
+  },
+  {label: "Value_35", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,3,5,5,8,6,7,4,3,6,6,5]
+  },
+  {label: "Value_36", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [2,6,7,5,7,5,3,5,3,8,8,3]
+  },
+  {label: "Value_37", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,3,4,8,7,8,8,5,1,5,5,5]
+  },
+  {label: "Value_38", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,1,1,5,8,4,1,4,3,5,7,6]
+  },
+  {label: "Value_39", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,3,4,8,8,5,3,3,3,5,6,6]
+  },
+  {label: "Value_40", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [1,5,5,9,9,6,1,3,9,10,10,6]
+  },
+  {label: "Value_41", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [2,4,6,7,5,4,3,4,4,3,3,5]
+  },
+  {label: "Value_42", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [4,4,6,7,5,5,6,6,8,8,9,4]
+  },
+  {label: "Value_43", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [3,4,5,10,8,2,2,3,8,6,4,2]
+  },
+  {label: "Value_44", 
+  backgroundColor: "rgba(0,0,200,0.2)",
+  data: [5,3,7,7,8,8,3,5,6,9,9,8]
+  }
+];
 var marksCanvas = document.getElementById("marksChart");
 
 var marksData = {
-  labels: ["Information Visualization", "Statistics", "Math", "Art", "Computer usage", "Programming", "Computer_graphics_programming", "HI interaction programming", "Communication", "Collaboration", "Code repository"],
+  labels: ["Information Visualization", "Statistics", "Math", "Art", "Computer usage", "Programming", "Computer_graphics_programming", "HI interaction programming", "User experience evaluation", "Communication", "Collaboration", "Code repository"],
   datasets: []
 };
+const setBg = () => {
+  const randomColor =  "#" + Math.floor(Math.random()*16777215).toString(16);
+  return randomColor
+}
 
 var chartDetails = {
   scale: {
@@ -27,6 +209,11 @@ var radarChart = new Chart(marksCanvas, {
   data: marksData,
   options: chartDetails
 });
+
+function clearRadarChart(){
+  marksData.datasets.splice(0,marksData.datasets.length)
+  radarChart.update();
+}
 // set the dimensions and margins of the graph
 var margin = {top: 30, right: 30, bottom: 20, left: 50},
     width = 700 - margin.left - margin.right,
@@ -66,15 +253,16 @@ d3.csv("https://raw.githubusercontent.com/actoract/IVIS-Project-1/main/public/Se
     .range([ 0, height ])
     .padding([0.1])
   svg.append("g")
-    .call(d3.axisLeft(y))
-    .on('click', function (d) {
+    .call(d3.axisLeft(y));
+    /*.on('click', function (d) {
       //alert(d3.axisLeft(y))
+      alert(d.group)
       marksData.datasets.push({
         label: "gggg", 
         backgroundColor: "rgba(0,0,200,0.2)",
         data: [5, 9, 6, 7, 7, 7, 9, 9, 9, 10,10]})
         radarChart.update();
-    });
+    });*/
 
   // color palette = one color per subgroup
   var color = d3.scaleOrdinal()
@@ -122,11 +310,14 @@ d3.csv("https://raw.githubusercontent.com/actoract/IVIS-Project-1/main/public/Se
                  .style("opacity", 0);
        })
        .on('click', function (d) {
-        alert("fbndfk")
+        //alert(d.data.group)
+        //alert(participants[0].label)
+        let single_value = participants.find(participant => participant.label == d.data.group)
+        //alert(single_value.label)
         marksData.datasets.push({
-          label: "gggg", 
-          backgroundColor: "rgba(0,0,200,0.2)",
-          data: [5, 9, 6, 7, 7, 7, 9, 9, 9, 10,10]})
+          label: single_value.label, 
+          backgroundColor: "rgba(" + Math.floor(Math.random() * (255 - 0 + 1) + 0) + "," + Math.floor(Math.random() * (255 - 0 + 1) + 0) + ","+ Math.floor(Math.random() * (255 - 0 + 1) + 0)+ ",0.5)",
+          data: single_value.data})
           radarChart.update();
       })
         /*.on('click', function (d, i) {

e6c7696709ffb3b4a42a23a929ecdf4053bec37e
anna <anna>
2022-01-25 17:53:25 +0100

    updates in port

--- server.js
+++ server.js
@@ -24,10 +24,8 @@ app.get('../skills.json', (req, res) => {
 })
 
 
-app.listen("3000", () => {
-    console.log('listening......');
-})
-
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."))
 
 
 

070c579c26465215768befbd000ff3729f959a0b
anna <anna>
2022-01-25 02:50:03 +0100

    added stacked barchart, json file to test spider chart vis

--- public/skills.js
+++ public/skills.js
@@ -1,9 +1,11 @@
-
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 30, bottom: 40, left: 90},
-    width = 800 - margin.left - margin.right,
+var margin = {top: 30, right: 30, bottom: 20, left: 50},
+    width = 700 - margin.left - margin.right,
     height = 700 - margin.top - margin.bottom;
 
+var div = d3.select("body").append("div")
+    .attr("class", "tooltip-donut")
+    .style("opacity", 0);
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
   .append("svg")
@@ -14,58 +16,88 @@ var svg = d3.select("#my_dataviz")
           "translate(" + margin.left + "," + margin.top + ")");
 
 // Parse the Data
-d3.csv("https://raw.githubusercontent.com/actoract/IVIS-Project-1/main/public/Self-Introduction-to-IVIS22-_Responses_-_2_%20(3).csv", function(data) {
+d3.csv("https://raw.githubusercontent.com/actoract/IVIS-Project-1/main/public/Self-Introduction-to-IVIS22-_Responses_-_2_%20(5).csv", function(data) {
 
-    var subgroups = data.rows.slice(1)
-    alert(subgroups)
+  // List of subgroups = header of the csv files = soil condition here
+  var subgroups = data.columns.slice(1)
+
+  // List of groups = species here = value of the first column called group -> I show them on the X axis
+  var groups = d3.map(data, function(d){return(d.group)}).keys()
   // Add X axis
   var x = d3.scaleLinear()
-    .domain([0, 120])
-    .range([ 0, width]);
+    .domain([0, 90])
+    .range([0, width]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
+    .call(d3.axisBottom(x).tickSizeOuter(0));
 
-const y_array = Array.from({ length: 44 }, (v, i) =>  i + 1); 
-  // Y axis
+  // Add Y axis
   var y = d3.scaleBand()
+  .domain(data.map(function(d) { return d.group; }))
     .range([ 0, height ])
-    .domain(y_array.map(function(i) { return i }))
-    .padding(.1);
+    .padding([0.1]);
   svg.append("g")
-    .call(d3.axisLeft(y))
+    .call(d3.axisLeft(y));
 
-  //Bars
-  svg.selectAll("myRect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", x(0) )
-    .attr("y", function(d) { return y(d.Information_Visualization_skills); })
-    .attr("width", function(d) { return x(d.Information_Visualization_skills); })
-    .attr("height", y.bandwidth() )
-    .attr("fill", "#69b3a2")
+  // color palette = one color per subgroup
+  var color = d3.scaleOrdinal()
+    .domain(subgroups)
+    .range(['#d81eee ','#bdda00 ','#b235a3 ','#c55373 ','#ea82a1 ','#64c9a0','#6988cf ','#c8f748 ','#84105a ','#b3cd4b ','#cfacb9 ','#18cff3 ', ])
 
+  //stack the data? --> stack per subgroup
+  var stackedData = d3.stack()
+    .keys(subgroups)
+    (data)
 
-    svg.selectAll("myRect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", x(0) )
-    .attr("y", function(d) { return y(d.Country); })
-    .attr("width", function(d) { return x(d.Value); })
-    .attr("height", y.bandwidth() )
-    .attr("fill", "#69b3a2")
-
-
-
-    // .attr("x", function(d) { return x(d.Country); })
-    // .attr("y", function(d) { return y(d.Value); })
-    // .attr("width", x.bandwidth())
-    // .attr("height", function(d) { return height - y(d.Value); })
-    // .attr("fill", "#69b3a2")
-
+  // Show the bars
+  svg.append("g")
+    .selectAll("g")
+    // Enter in the stack data = loop key per key = group per group
+    .data(stackedData)
+    .enter().append("g")
+      .attr("fill", function(d) { return color(d.key); })
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data(function(d) { return d; })
+      .enter().append("rect")
+        .attr("x", function(d) {return x(d[0]); })
+        .attr("y", function(d) { return y(d.data.group); })
+        .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+        .attr("height",y.bandwidth())
+        .on('mouseover', function (d, i) {
+            d3.select(this).transition()
+                 .duration('50')
+                 .attr('opacity', '.85');
+            div.transition()
+                 .duration(50)
+                 .style("opacity", 1);
+            let num = (Math.round((d.value / d.data.all) * 100)).toString() + '%';
+            div.html("d.data.group")
+                 .style("left", (d3.event.pageX + 10) + "px")
+                 .style("top", (d3.event.pageY - 15) + "px");
+       })
+       .on('mouseout', function (d, i) {
+            d3.select(this).transition()
+                 .duration('50')
+                 .attr('opacity', '1');
+            div.transition()
+                 .duration('50')
+                 .style("opacity", 0);
+       })
+       .on('click', function (d, i) {
+           //alert(i)
+           mouseClick(i)
+        });
 })
+
+function mouseDown() {
+    console.log("hkjfhsk");
+}
+
+function mouseUp(name) {
+    console.log(name);
+}
+
+function mouseClick(i) {
+    console.log(i);
+}
